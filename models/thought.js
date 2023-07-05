9225b94a22835d3924c77ca46d1229d5fe51f883
Chris Gramer <gramer3514@gmail.com>
2023-07-04 19:16:48 -0700

    updated thought model

---
+++ models/thought.js
@@ -0,0 +1,39 @@
+const { Schema, model } = require('mongoose');
+const reactionSchema = require('./reaction');
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+        type: String,
+        required: true, 
+        minLength: 1, 
+        maxLength: 280,
+        },
+    
+        createdAt: {
+            type: Date,
+            default: Date.new,
+            get: timestamp => new Date(timestamp).toLocaleDateString(),
+        },
+
+        username: {
+            type: String,
+            required: true,
+        },
+        reactions: [reactionSchema]
+    },
+    {
+        toJSON: {
+            getters: true, 
+            virtuals: true,
+        }
+    }
+);
+
+thoughtSchema.virtual('reactionCount').get(function(){
+    return this.reactions.length;
+});
+
+const thought = model('thought', thoughtSchema)
+
+module.exports = thought
