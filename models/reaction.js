1c1f76cf0bd70950a4a2ec44cb8add313876e86a
Chris Gramer <gramer3514@gmail.com>
2023-07-04 18:21:07 -0700

    updated reaction model

--- models/reaction.js
+++ models/reaction.js
@@ -1,2 +1,31 @@
 const { Schema, Types } = require('mongoose');
 
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new ObjectId,
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            macLength: 280,
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: timestamp => new Date(timestamp).toLocaleDateString()
+        }
+    },
+    {
+        toJSON: {
+            getters: true,
+        }
+    }
+)
+
+module.exports= reactionSchema;
