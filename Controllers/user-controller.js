d86d5c11c5737b47ba924c31a8fe72e00c080b26
Chris Gramer <gramer3514@gmail.com>
2023-07-05 21:17:00 -0700

    completed updateUser

--- Controllers/user-controller.js
+++ Controllers/user-controller.js
@@ -21,7 +21,14 @@ const UserController = {
     },
 
     updateUser(req, res) {
-        User.findOneAndUpdate
+        User.findOneAndUpdate(req.params.id, req.body, {new: true})
+        .then(userInfo => {
+            if(!userInfo) {
+                return res.status(404).json({message: 'User not found'});
+            }
+            res.json(userInfo)
+        })
+        .catch(err => res.status(500).json(err));
     }
 
 }
