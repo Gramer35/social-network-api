8d1eb0cd6a2d93feea63099a5afc4a8ea100a422
Chris Gramer <gramer3514@gmail.com>
2023-07-10 17:13:29 -0700

    updated the connection to actually connect

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const mongoConnect = process.env.MONGODB_URI || 'mongodb:localhost:27017/socialNetworkAPI'
+const mongoConnect = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/socialNetworkAPI'
 
 mongoose.connect(mongoConnect);
 
--- server.js
+++ server.js
@@ -12,6 +12,6 @@ app.use(routes);
 
 db.once('open', () => {
     app.listen(PORT, () => {
-        console.log('Running on port ${PORT}');
+        console.log(`Running on port ${PORT}`);
     })
 });
